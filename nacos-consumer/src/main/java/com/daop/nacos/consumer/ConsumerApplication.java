82ec73122a2eef74fcf2f82b278a14af1267fb16
Gadmind <daopudu@sina.com>
2020-09-28 17:02:34 +0800

    开启Feign配置

--- nacos-consumer/src/main/java/com/daop/nacos/consumer/ConsumerApplication.java
+++ nacos-consumer/src/main/java/com/daop/nacos/consumer/ConsumerApplication.java
@@ -1,48 +1,18 @@
 package com.daop.nacos.consumer;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class ConsumerApplication {
-    @LoadBalanced
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
-    @RestController
-    class TestController {
-        private final RestTemplate restTemplate;
 
-        @Value("${params.name}")
-        private String paramsName;
-
-        @Value("${params.desc}")
-        private String paramsDesc;
-
-        @Autowired
-        public TestController(RestTemplate restTemplate) {
-            this.restTemplate = restTemplate;
-        }
-
-        @GetMapping("/echo/{str}")
-        public String echo(@PathVariable String str) {
-            return restTemplate.getForObject("http://nacos-provider/echo/" + str, String.class)+"\tParamsName:"+paramsName+"\tparamsDesc:"+paramsDesc;
-        }
-    }
 }
