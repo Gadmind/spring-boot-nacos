2e0789ed5b969a5457f5e58405c23e98ec79830e
Gadmind <daopudu@sina.com>
2020-09-28 17:02:18 +0800

    Dubbo调用控制器

---
+++ nacos-consumer/src/main/java/com/daop/nacos/consumer/controller/DubboApiController.java
@@ -0,0 +1,25 @@
+package com.daop.nacos.consumer.controller;
+
+import com.daop.dubbo.api.DubboEchoService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @BelongsProject: spring-cloud-nacos
+ * @BelongsPackage: com.daop.nacos.consumer.controller
+ * @Description: DubboAPI调用请求
+ * @DATE: 2020-09-28
+ * @AUTHOR: Administrator
+ **/
+@RestController
+public class DubboApiController {
+    @DubboReference
+    private DubboEchoService echoService;
+
+    @GetMapping("/dubbo/echo/{msg}")
+    public String dubboEcho(@PathVariable("msg") String msg) {
+        return echoService.echoService(msg);
+    }
+}
